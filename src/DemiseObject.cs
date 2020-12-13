8b73e730c85854f68d85cf2fe1dca10c41b68962
FyiurAmron <spamove@gmail.com>
2020-12-13 22:52:48 +0100

    first attempt as decoding the headers DEmise 3D Objects

--- src/DemiseObject.cs
+++ src/DemiseObject.cs
@@ -21,14 +21,62 @@ public class DemiseObject : DemiseAsset {
         }
 
         short matCount = br.ReadInt16();
-        short objCount = br.ReadInt16(); // ??
+        short groupCount = br.ReadInt16(); // ??
 
-        if ( objCount > 1 ) {
-            Console.Out.Write( $"{fileUtil.name} matCount: {matCount} u2: {objCount}\n" );
+        if ( groupCount > 1 ) {
+            Console.Out.Write( $"{fileUtil.name} matCount: {matCount} u2: {groupCount}\n" );
         }
+
+        Map<string, DemiseObjectMaterial> materials = new();
+        for ( int i = 0; i < matCount; i++ ) {
+            DemiseObjectMaterial mat = new();
+            mat.name = br.readString( 32 );
+            mat.textureFilename = br.readString( 32 );
+            br.ReadByte(); // R
+            br.ReadByte(); // G
+            br.ReadByte(); // B
+            br.ReadByte(); // R
+            br.ReadByte(); // G
+            br.ReadByte(); // B
+            Console.Out.Write( br.ReadInt32() + " " ); // always 0?
+            Console.Out.Write( br.ReadInt32() + "\n" ); // always 0?
+            br.ReadSingle();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+        }
+
+        for ( int i = 0; i < groupCount; i++ ) {
+            DemiseObjectGroup grp = new();
+            grp.name = br.readString( 32 );
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadInt16();
+            br.ReadSingle();
+            br.ReadSingle();
+            br.ReadSingle();
+            br.ReadSingle();
+            // br.ReadSingle();
+            // br.ReadSingle();
+        }
+        // now the frames begin: 32float, 32b string
 
         return ms.Position;
     }
+}
+
+public class DemiseObjectMaterial {
+    public string name;
+    public string textureFilename;
+}
+
+public class DemiseObjectGroup {
+    public string name;
 }
 
 }
